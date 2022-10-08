5682139e522188bb1e9578c2544a5f0cbf690fd8
Ernesto <ernesboyanos@hotmail.com>
2022-10-08 20:50:33 -0300

    todo

---
+++ src/components/listar-crud.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+   
+    <div class="card">
+      <div class="card-header">
+        Empleados
+      </div>
+      <div class="card-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NOMBRE</th>
+              <th>CORREO</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="empleado in empleados" :key="empleado.id">
+              <td>{{empleado.id}}</td>
+              <td>{{empleado.nombre}}</td>
+              <td>{{empleado.correo}}</td>
+              <td>
+
+                <a name="" id="" class="btn btn-primary" href="#" role="button">Editar</a>
+                <a name="" id="" class="btn btn-primary" href="#" role="button">Borrar</a>
+
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      empleados:[]
+    }
+
+  },
+  created:function(){
+
+    this.consutarempleados();
+
+  },
+  methods:{
+    consutarempleados(){
+      fetch('http://localhost/Servicios/').then(respuesta=>respuesta.json())
+      .then((datosRespuesta)=>{
+      console.log(datosRespuesta)
+        this.empleados=[]
+        if(typeof datosRespuesta[0].success==='undefined'){
+          this.empleados=datosRespuesta;
+        }
+        
+
+
+      })
+      .catch(console.log)
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+import crearCrud from '../components/crear-crud.vue'
+import editarCrud from '../components/editar-crud.vue'
+import listarCrud from '../components/listar-crud.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/crear',
+    name: 'crear',
+    component: crearCrud
+  },
+  {
+    path: '/editar',
+    name: 'editar',
+    component: editarCrud
+  },
+  {
+    path: '/listar',
+    name: 'listar',
+    component: listarCrud
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
